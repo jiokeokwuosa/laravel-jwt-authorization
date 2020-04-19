01bbf7cd006cacb117aedc8c1180e4fe3c7c895c
jiokeokwuosa <okwuosachijioke@gmail.com>
2020-04-19 20:48:49 +0100

    Initial Commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use JWTAuth;
+use JWTAuthException;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        return "we are here bro";
+    }
+    
+    private function getToken($email, $password)
+    {
+        $token = null;       
+        try {
+            if (!$token = JWTAuth::attempt( ['email'=>$email, 'password'=>$password])) {
+                return response()->json([
+                    'response' => 'error',
+                    'message' => 'Password or email is invalid',
+                    'token'=>$token
+                ]);
+            }
+        } catch (JWTAuthException $e) {
+            return response()->json([
+                'response' => 'error',
+                'message' => 'Token creation failed',
+            ]);
+        }
+        return $token;
+    }
+
+    public function login(Request $request)
+    {
+        $user = \App\User::where('email', $request->email)->get()->first();
+        if ($user && \Hash::check($request->password, $user->password)) // The passwords match...
+        {
+            $token = self::getToken($request->email, $request->password);            
+            $response = ['success'=>true, 'data'=>['id'=>$user->id,'auth_token'=>$token,'name'=>$user->name, 'email'=>$user->email]];           
+        }
+        else 
+          $response = ['success'=>false, 'data'=>'Record doesnt exists'];
+      
+
+        return response()->json($response, 201);
+    }
+
+    
+    public function register(Request $request)
+    {        
+        //The token is passed as authorization bearer
+        $payload = [
+            'password'=>\Hash::make($request->password),
+            'email'=>$request->email,
+            'name'=>$request->name           
+        ];
+                  
+        $user = new \App\User($payload);
+        if ($user->save())
+        {
+            
+            $token = self::getToken($request->email, $request->password); // generate user token
+            
+            if (!is_string($token))  return response()->json(['success'=>false,'data'=>'Token generation failed'], 201);
+               
+            $response = ['success'=>true, 'data'=>['name'=>$user->name,'id'=>$user->id,'email'=>$request->email,'auth_token'=>$token]];        
+        }
+        else
+            $response = ['success'=>false, 'data'=>'Couldnt register user'];
+        
+        
+        return response()->json($response, 201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+                if (! $user = JWTAuth::parseToken()->authenticate()) {
+                        return response()->json(['user_not_found'], 404);
+                }
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+                return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+                return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+                return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        return response()->json(compact('user'));
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+//api-header middleware is needed to append a 
+//Access-Control-Allow-Origin header to all responses from the server
+Route::group(['middleware' => ['jwt-auth','api-header']], function () {
+  
+    // all routes to protected resources are registered here  
+    Route::get('users/list', function(){
+        $users = App\User::all();
+        
+        $response = ['success'=>true, 'data'=>$users];
+        return response()->json($response, 201);
+    });
+
+    Route::get('user/thisuser','UsersController@getAuthenticatedUser');
+    
+});
+
+Route::group(['middleware' => 'api-header'], function () {
+  
+    // The registration and login requests doesn't come with tokens 
+    // as users at that point have not been authenticated yet
+    // Therefore the jwtMiddleware will be exclusive of them
+
+    Route::post('user/login', 'UsersController@login');
+    Route::post('user/register', 'UsersController@register');
+});
+
+Route::get('me', 'UsersController@index');
